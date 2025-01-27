c86ae61d96abeeeac8f95ce737af51f1b042a8b3
Bari328 <2898830144@qq.com>
2025-01-27 08:10:57 +0800

    Update 卡牌 洛谷 - P8800 .cpp

--- ACM寒假集训/第二节/卡牌 洛谷 - P8800 .cpp
+++ ACM寒假集训/第二节/卡牌 洛谷 - P8800 .cpp
@@ -1,7 +1,9 @@
+思路：对于卡牌；只要找出a[i]+b[i]的最小值，以此作为r的起始值，这样就不用管b[i]的限制
+	之后利用二分答案的模板就可以完成。
 #include<iostream>
 #include<algorithm>
 using namespace std;
-long long n,m,a[200010],b[200010];
+long long n,m,a[200010],b[200010],M;
 bool check(long long mid)
 {	
 	long long sum=0;
@@ -13,15 +15,15 @@ bool check(long long mid)
 	else return false;
  }
 int main()
-{	long long m=10000000;
+{	long long M=10000000;
 	cin>>n>>m;
 	for(int i=1;i<=n;i++)
 		cin >> a[i];
 	for(int i=1;i<=n;i++)
 		cin >> b[i];
 	for(int i=1;i<=n;i++)
-		m=min(m,a[i]+b[i]);
-	long long l=1,r=m+1;
+		M=min(M,a[i]+b[i]);
+	long long l=1,r=M;
 	while(l<r)
 	{
 		int mid=(l+r+1)/2;
@@ -31,3 +33,4 @@ int main()
 	cout<<l;
 	 
 }
+总结:记住变量别重复了|——|
