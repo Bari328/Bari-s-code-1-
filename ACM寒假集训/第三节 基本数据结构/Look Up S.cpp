0356cfe69d3ed01c3395132f72cc9dc26954402e
Bari328 <2898830144@qq.com>
2025-02-07 23:43:17 +0800

    Update Look Up S.cpp

--- ACM寒假集训/第三节 基本数据结构/Look Up S.cpp
+++ ACM寒假集训/第三节 基本数据结构/Look Up S.cpp
@@ -1,3 +1,8 @@
+本题思路：常规思路：暴力解法两次循环遍历时间复杂度o（n^2）
+	遂想要优化算法。
+	优化思路：向右看齐说明在一定区间内升高是类递减的
+		那么有极大可能相邻的类递减数的仰望对象是相同的
+		所以利用栈的后进先出的特点优化算法。
 #include<iostream>
 #include<stack>
 using namespace std;
@@ -30,7 +35,7 @@ int main()
 				ans[s.top().num]=a[i].num;
 				s.pop();
 			}
-			s.push(a[i]);
+			s.push(a[i]);//标注一处地点
 		}
 	}
 	while(!s.empty())
@@ -44,3 +49,4 @@ int main()
 	}
 	return 0;
 }
+本题反思：要记得删元素啊，还有记得s.push(a[i]);
